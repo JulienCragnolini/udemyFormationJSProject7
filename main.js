2450a1128caadfe18b3aa698b0678d61ab8b3148
Julien Cragnolini <cragnolini68.j@gmail.com>
2020-05-11 09:17:24 +0200

    v3.0

--- main.js
+++ main.js
@@ -1,44 +1,27 @@
-// Cette variable vaut false ce qui veut dire que par défaut, nous ne reproposons pas un calcul.
-let restart = false; 
-do {    
-    // Demande un choix de programme
-    do {
-        var choice = prompt('Choissisez un programme\n\nAddition = 1\nSoustraction = 2 \nDivision = 3 \nMultiplication = 4\n');
-    } while (choice != 1 && choice != 2  && choice != 3 && choice != 4)
+let bouton = document.querySelector('#mode');
+let span = document.querySelector('span');
 
-    // Demande deux nombres
-    do {
-        var firstNbr = Number(prompt('Premier nombre :'));
-        var secondNbr = Number(prompt('Second nombre :'));
-    } while (isNaN(firstNbr) || isNaN(secondNbr) || firstNbr == '' || secondNbr == '')
-
-    // Appelle la fonction concernée
-    try {
-        switch (choice) {
-            case '1':
-                var result = addition(firstNbr, secondNbr);
-                break;
-            case '2':
-                var result = soustraction(firstNbr, secondNbr);
-                break;
-            case '3':
-                var result = division(firstNbr, secondNbr);
-                break;
-            case '4':
-                var result = multiplication(firstNbr, secondNbr);
-                break;
-            default:
-                throw new Error('Une erreur est survenue.');
-        }    
-        // Affiche le résultat
-        alert(`Programme choisi : ${choice}\nPremier nombre : ${firstNbr}\nSecond nombre : ${secondNbr}\nRésultat : ${result}`);
+if (localStorage.getItem('theme')) {
+    if (localStorage.getItem('theme') == 'sombre') {
+        modeSombre();
     }
-    catch(error) {
-        // Affiche l'erreur
-        alert(error); 
-    }
+}
 
-    // On demande grâce à la boîte de dialogue confirm() si l'utilisateur veut recommencer.
-    restart = confirm("Souhaitez-vous recommencer un calcul ?"); 
-    
-} while(restart)
+bouton.addEventListener('click', () => {
+    if(document.body.classList.contains('dark')){
+        // Mode clair
+        document.body.className = '';
+        span.textContent = 'Thème sombre';
+        localStorage.setItem('theme', 'clair');
+    }
+    else {
+        // Mode sombre
+        modeSombre();
+    }
+})
+
+function modeSombre() {
+    document.body.className = 'dark';
+    span.textContent = 'Thème clair';
+    localStorage.setItem('theme', 'sombre');
+}
