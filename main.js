02ab75bd33491b9e4f80156b7c2b89f7c4321f11
Julien Cragnolini <cragnolini68.j@gmail.com>
2020-05-11 09:09:32 +0200

    v1.0

---
+++ main.js
@@ -0,0 +1,27 @@
+let bouton = document.querySelector('#mode');
+let span = document.querySelector('span');
+
+if (localStorage.getItem('theme')) {
+    if (localStorage.getItem('theme') == 'sombre') {
+        modeSombre();
+    }
+}
+
+bouton.addEventListener('click', () => {
+    if(document.body.classList.contains('dark')){
+        // Mode clair
+        document.body.className = '';
+        span.textContent = 'Thème sombre';
+        localStorage.setItem('theme', 'clair');
+    }
+    else {
+        // Mode sombre
+        modeSombre();
+    }
+})
+
+function modeSombre() {
+    document.body.className = 'dark';
+    span.textContent = 'Thème clair';
+    localStorage.setItem('theme', 'sombre');
+}
